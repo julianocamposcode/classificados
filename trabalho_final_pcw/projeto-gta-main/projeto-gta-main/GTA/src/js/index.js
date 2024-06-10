2b1cb386af3d49b5dd2dabb72c2b3b9087b6fca9
Juliano Campos <88381643+julianocamposcode@users.noreply.github.com>
2024-06-10 20:02:10 -0300

    Delete trabalho_final_pcw directory

--- trabalho_final_pcw/projeto-gta-main/projeto-gta-main/GTA/src/js/index.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('.btn-plataforma')
-const plataformas = document.querySelector('.btn-plataforma .plataformas')
-
-btn.onclick = () => {
-    plataformas.classList.toggle('ativo')
-}
-
